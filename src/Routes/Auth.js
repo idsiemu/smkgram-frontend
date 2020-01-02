ed6dda05e09954056361dfcebcafd638d7537497
SMK <idsiemu@gmail.com>
2020-01-02 20:58:38 +0900

    Hooks 세팅 onChange 리스너같은 개념

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,7 +1,9 @@
 import React, {useState} from "react";
 import styled from "styled-components";
-import Input from "../Components/Input"
-import Button from "../Components/Button"
+import Input from "../Components/Input";
+import Button from "../Components/Button";
+import useInput from "../Hooks/useInput";
+
 
 const Wrapper = styled.div`
     min-height: 80vh;
@@ -48,22 +50,28 @@ const Form = styled(Box)`
 
 export default () => {
     const [action, setAction] = useState("logIn");
+    const name = useInput("");
+    const password = useInput("");
+    const firstName = useInput("");
+    const lastName = useInput("");
+    const email = useInput("");
+
     return (
         <Wrapper>
             <Form>
                 {action === "logIn" ? (
                     <form>
-                        <Input placeholder={"Name"}/>
-                        <Input placeholder={"Password"}/>
+                        <Input placeholder={"Name"} {...name}/>
+                        <Input placeholder={"Password"} {...password} type="password"/>
                         <Button text={"Log in"}/>
                     </form>
                 ) : (
                     <form>
-                        <Input placeholder={"First name"}/>
-                        <Input placeholder={"Last name"}/>
-                        <Input placeholder={"Email"}/>
-                        <Input placeholder={"Name"}/>
-                        <Input placeholder={"Password"}/>
+                        <Input placeholder={"First name"} {...firstName}/>
+                        <Input placeholder={"Last name"} {...lastName}/>
+                        <Input placeholder={"Email"} {...email} type="email"/>
+                        <Input placeholder={"Name"} {...name}/>
+                        <Input placeholder={"Password"} {...password} type="password"/>
                         <Button text={"Sign up"}/>
                     </form>
                 )}
