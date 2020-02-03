8bed0628474c35d680889850c0dc23b837a237d2
SMK <idsiemu@gmail.com>
2020-02-03 22:24:00 +0900

    start profile

--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -1,1 +1,20 @@
-export default () => "Profile";
+import React from "react";
+import styled from "styled-components";
+import { gql } from "apollo-boost";
+
+const GET_USER = gql`
+    query seeUser($name: String!) {
+        seeUser(name:$name){
+            id
+            avatar
+            name
+            fullName
+            isFollowing
+            isSelf
+            bio
+            followingCount
+            followersCount
+            posts
+        }
+    }
+`;
